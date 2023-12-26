a395d545732f7ffec666e373727e07d0b4ad7df3
phamthanhcong8888 <congptth2209010@fpt.edu.vn>
2023-12-26 09:34:09 +0700

    mvc

---
+++ MvcShop/MvcShop/Controllers/ShopsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcShop.Data;
+using MvcShop.Models;
+
+namespace MvcShop.Controllers
+{
+    public class ShopsController : Controller
+    {
+        private readonly MvcShopContext _context;
+
+        public ShopsController(MvcShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shops
+        public async Task<IActionResult> Index()
+        {
+              return _context.Shop != null ? 
+                          View(await _context.Shop.ToListAsync()) :
+                          Problem("Entity set 'MvcShopContext.Shop'  is null.");
+        }
+
+        // GET: Shops/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Shop == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context.Shop
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            return View(shop);
+        }
+
+        // GET: Shops/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shops/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Shop shop)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shop);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shop);
+        }
+
+        // GET: Shops/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Shop == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context.Shop.FindAsync(id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+            return View(shop);
+        }
+
+        // POST: Shops/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Shop shop)
+        {
+            if (id != shop.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shop);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShopExists(shop.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shop);
+        }
+
+        // GET: Shops/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Shop == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context.Shop
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            return View(shop);
+        }
+
+        // POST: Shops/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Shop == null)
+            {
+                return Problem("Entity set 'MvcShopContext.Shop'  is null.");
+            }
+            var shop = await _context.Shop.FindAsync(id);
+            if (shop != null)
+            {
+                _context.Shop.Remove(shop);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShopExists(int id)
+        {
+          return (_context.Shop?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ MvcShop/MvcShop/Data/MvcShopContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcShop.Models;
+
+namespace MvcShop.Data
+{
+    public class MvcShopContext : DbContext
+    {
+        public MvcShopContext (DbContextOptions<MvcShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcShop.Models.Shop> Shop { get; set; } = default!;
+    }
+}
