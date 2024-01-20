d48279e7d231dfaac1c7c6f438956d55e56195a5
phamthanhcong8888 <congptth2209010@fpt.edu.vn>
2024-01-20 10:11:07 +0700

    Exam ASP.NET MVC
    Company

---
+++ Company/Company/Data/CompanyContext.cs
@@ -0,0 +1,18 @@
+﻿using Company.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+
+namespace Company.Data
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext(DbContextOptions<CompanyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
